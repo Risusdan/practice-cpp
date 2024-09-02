c0092b9ee552e77519df2ff672d0b0fce4b974f5
ChienLinSu <dan199537@gmail.com>
2024-09-02 23:56:05 +0800

    test: ignore all the example test cases
    - also update clang-format config for cpp specific modifiers

--- src/example/trangle_test.cpp
+++ src/example/trangle_test.cpp
@@ -1,40 +1,37 @@
-#include "trangle.h"
 #include <gtest/gtest.h>
+#include "trangle.h"
 
-namespace
-{
-TEST(TriangleTest, InvalidInput)
+TEST(DISABLED_TriangleTest, InvalidInput)
 {
     EXPECT_EQ(Invalid, GetTriangleType(-10, 20, 30));
     EXPECT_EQ(Invalid, GetTriangleType(10, -20, 30));
     EXPECT_EQ(Invalid, GetTriangleType(3, 4, -8));
 }
 
-TEST(TriangleTest, NonTriangle)
+TEST(DISABLED_TriangleTest, NonTriangle)
 {
     EXPECT_EQ(Invalid, GetTriangleType(1, 1, 2));
     EXPECT_EQ(Invalid, GetTriangleType(10, 20, 30));
     EXPECT_EQ(Invalid, GetTriangleType(3, 4, 8));
 }
 
-TEST(TriangleTest, Equilateral)
+TEST(DISABLED_TriangleTest, Equilateral)
 {
     EXPECT_EQ(Equilateral, GetTriangleType(5, 5, 5));
     EXPECT_EQ(Equilateral, GetTriangleType(10, 10, 10));
     EXPECT_EQ(Equilateral, GetTriangleType(15, 15, 15));
 }
 
-TEST(TriangleTest, Isosceles)
+TEST(DISABLED_TriangleTest, Isosceles)
 {
     EXPECT_EQ(Isosceles, GetTriangleType(5, 5, 6));
     EXPECT_EQ(Isosceles, GetTriangleType(10, 10, 12));
     EXPECT_EQ(Isosceles, GetTriangleType(15, 15, 18));
 }
 
-TEST(TriangleTest, Scalene)
+TEST(DISABLED_TriangleTest, Scalene)
 {
     EXPECT_EQ(Scalene, GetTriangleType(5, 6, 7));
     EXPECT_EQ(Scalene, GetTriangleType(10, 11, 12));
     EXPECT_EQ(Scalene, GetTriangleType(15, 16, 17));
 }
-} // namespace
